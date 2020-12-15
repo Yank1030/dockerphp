b96323f3f83e5e0eac2edcaf59b5adb48ff78cdd
kawanoyasunori <kawano.yasunori@relation2012.com>
2020-12-16 02:28:31 +0900

    データ複数

--- server/twig/sample.php
+++ server/twig/sample.php
@@ -7,8 +7,15 @@ $twig = new \Twig\Environment($loader);
 
 $template = $twig->load('sample.html.twig');
 
+
+$data2 = array(
+    'title' => 'sample2',
+    'message'  => 'My Webpage2!',
+);
+
 $data = array(
     'title' => 'sample',
     'message'  => 'My Webpage!',
+    'data' => $data2
 );
 echo $template->render($data);
